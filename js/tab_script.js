59af92cc8d355e3c548ba35b413daf030a47a9a6
朴英蘭 <103724240+yonnana47@users.noreply.github.com>
2022-05-04 15:53:53 +0900

    Add files via upload

---
+++ js/tab_script.js
@@ -0,0 +1,32 @@
+$(function(){
+	
+	var acc = document.getElementsByClassName("accordion");
+	var i;
+
+	for (i = 0; i < acc.length; i++) {
+	  acc[i].addEventListener("click", function() {
+		this.classList.toggle("active");
+		var panel = this.nextElementSibling;
+		if (panel.style.maxHeight) {
+		  panel.style.maxHeight = null;
+		} else {
+		  panel.style.maxHeight = panel.scrollHeight + "px";
+		} 
+	  });
+	}
+
+});
+
+function openBoard(evt, boardName) {
+	  var i, x, tablinks;
+	  x = document.getElementsByClassName("board");
+	  for (i = 0; i < x.length; i++) {
+		x[i].style.display = "none";
+	  }
+	  tablinks = document.getElementsByClassName("tablink");
+	  for (i = 0; i < x.length; i++) {
+		tablinks[i].className = tablinks[i].className.replace(" opacity", "");
+	  }
+	  document.getElementById(boardName).style.display = "block";
+	  evt.currentTarget.className += " opacity";
+}
